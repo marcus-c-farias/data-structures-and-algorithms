222dc9e50ff9ebf8fe9f757bd0fe1fd162f509dc
Marcus Farias <marcusczupriniakifarias@gmail.com>
2025-05-26 17:44:39 -0300

    recursion

---
+++ data-structures-and-algorithms/Recursion/Exercices.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    internal class Exercices
+    {
+        public int Factorial(int value)
+        {
+            if (value == 1)
+                return 1;
+
+            return value * this.Factorial(value - 1);
+        }
+
+        public int Fibonacci(int value)
+        {
+            if (value <= 0)
+                return 0;
+
+            if (value == 1)
+                return 1;
+
+            return Fibonacci(value - 1) + Fibonacci(value - 2);
+        }
+
+        public string Reverse(string s)
+        {
+            if (s.Length > 0)
+            {
+                string substring = s.Substring(1);
+                return this.Reverse(substring) + s[0];
+            }
+
+            return s;
+        }
+    }
+}
---
+++ data-structures-and-algorithms/Recursion/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Recursion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var exercises = new Exercices();
+
+            var result = exercises.Factorial(6);
+            result = exercises.Fibonacci(10);
+
+            
+            var reversedString = exercises.Reverse("marcus");
+        }
+    }
+}
